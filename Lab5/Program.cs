3f7245372198c1b763eddce8a0fb951d24c2be17
KyryloPanchenko <w71458@student.wsiz.edu.pl>
2025-12-01 14:37:20 +0100

    Update Program.cs

--- Lab5/Program.cs
+++ Lab5/Program.cs
@@ -1,1 +1,24 @@
+using System;
+using System.IO;
 
+class Program
+{
+    static void Main()
+    {
+        string filePath = "dane.txt";
+
+        int liczbaPytan = 5;
+
+        using (StreamWriter writer = new StreamWriter(filePath))
+        {
+            for (int i = 0; i < liczbaPytan; i++)
+            {
+                Console.WriteLine($"Podaj tekst #{i + 1}:");
+                string input = Console.ReadLine();
+                writer.WriteLine(input);
+            }
+        }
+
+        Console.WriteLine($"Wszystkie dane zostały zapisane do pliku: {filePath}");
+    }
+}
